261ba8c323122e39a39a934a35cf3acc3f2bc69f
kimtaesu <taesu614@naver.com>
2024-02-19 21:46:31 +0900

    Update Playerinfo.cs

--- reset/Assets/Script/Map/Playerinfo.cs
+++ reset/Assets/Script/Map/Playerinfo.cs
@@ -17,16 +17,17 @@ public class Playerinfo : MonoBehaviour
         savedata = GameObject.Find("SaveData").GetComponent<SaveData>();
         playerhp.text = savedata.GetPlayerHealth().ToString();
         playerMoney.text = savedata.GetPlayerMoney().ToString();
-        hpwhite.transform.localScale = new Vector3(1-savedata.GetPlayerHealthPercent(),1f,1f);
-        if(savedata.GetPlayerHealth() <= 0)
+        hpwhite.transform.localScale = new Vector3(1 - savedata.GetPlayerHealthPercent(), 1f, 1f);
+        if (savedata.GetPlayerHealth() <= 0)
         {
             Destroy(GameObject.Find("SaveData"));
+            AudioManager.instance.DestroyAudioManager();
             SceneManager.LoadScene("Press2StartScene");
         }
     }
 
     public void SetPlayerInfo() //플레이어 정보 설정
-    {   
+    {
         savedata = GameObject.Find("SaveData").GetComponent<SaveData>();
         playerhp.text = savedata.GetPlayerHealth().ToString();
         playerMoney.text = savedata.GetPlayerMoney().ToString();
